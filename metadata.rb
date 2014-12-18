d2a2709886df8c87a839a9812176642b952613be
Matt Rieger <mrieger@mrskin.com>
2014-12-18 10:48:25 -0600

    Berkshelf create

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'test-tdi'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures test-tdi'
+long_description 'Installs/Configures test-tdi'
+version          '0.1.0'
